ec6642ee29bce7467ef7e0a123ed526bece309b6
Alexander Khristosenko <dr.claws.sakh@gmail.com>
2019-02-17 18:52:22 +1000

    Add problem description with qt

--- .travis/linux/before_install.sh
+++ .travis/linux/before_install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Because Xenial's repo doesn't have relatively new version
+# of Qt5 (since july 2015!) we have to use this repo.
 sudo add-apt-repository -y ppa:beineri/opt-qt-5.12.0-xenial
 
 sudo apt-get update
--- .travis/linux/install.sh
+++ .travis/linux/install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
+# Because of the problem with Qt described in "before_install.sh",
+# we use these packages. Not all dependencies are listed, so be caution.
 packages_list='qt512-meta-minimal qt512tools libgles2-mesa-dev libgl1-mesa-dev'
 
 if [ "$BUILD_TYPE" = "doxygen" ]; then 
 	packages_list+=' doxygen'
-elif [ "$BUILD_TYPE" = "coverage" ]; then
-	packages_list+=' ' #' libqt5sql5-sqlite'
 fi
 sudo apt-get install -y $packages_list
 
@@ -13,7 +13,6 @@ sudo apt-get install -y $packages_list
 if [ "$BUILD_TYPE" = "coverage" ]; then 
 	gem install lcoveralls
 fi
-
 
 source /opt/qt512/bin/qt512-env.sh
 	
