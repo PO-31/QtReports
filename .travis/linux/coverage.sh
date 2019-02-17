307c8f7b596a22bcbefe4233f74ca9dfad546d61
Alexander Khristosenko <dr.claws.sakh@gmail.com>
2019-02-17 18:17:37 +1000

    coverage badge generation enabled

--- .travis/linux/coverage.sh
+++ .travis/linux/coverage.sh
@@ -21,6 +21,7 @@ lcov --version
 lcov --capture --directory "$TRAVIS_BUILD_DIR/qtreports" --output-file coverage.info
 lcov --remove coverage.info --output-file coverage.info "*moc_*.cpp"
 lcov --remove coverage.info --output-file coverage.info "*/usr/*"
+lcov --remove coverage.info --output-file coverage.info "/opt/qt*"
 
 echo
 echo "Generating html view of coverage data"
@@ -29,9 +30,10 @@ git clone  -b gh-pages https://github.com/PO-31/QtReports ../gh_pages
 rm -r ../gh_pages/${TRAVIS_BRANCH}
 genhtml coverage.info --output-directory ../gh_pages/${TRAVIS_BRANCH}
 
-# echo "\nGenerating badge"
-# coverage_percent=$(lcov --summary coverage.info)
-# ./../gh_pages/covbadger/covbadger-linux coverage_percent >> ../gh_pages/${TRAVIS_BRANCH}/badge.svg
+echo
+echo "Generating badge"
+coverage_percent=$(lcov --summary coverage.info | egrep -m 1 -o '[0-9]+\.[0-9]')
+./../gh_pages/covbadger/covbadger-linux $coverage_percent >> ../gh_pages/${TRAVIS_BRANCH}/badge.svg
 
 echo
 echo "Pushing to gh-pages"
