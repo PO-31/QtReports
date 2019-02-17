9dd3e8ad2355c218a21fc10440c03ae6737b8f33
Alexander Khristosenko <dr.claws.sakh@gmail.com>
2019-02-17 16:56:29 +1000

    travis fix

--- .travis/linux/before_install.sh
+++ .travis/linux/before_install.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
 
-packages_list=''
 if [ "${CC}" = "clang" ]; then
-	sudo add-apt-repository -y ppa:beineri/opt-qt-5.12.0-xenial
-	packages_list='qt512-meta'
-else
-	packages_list='qt5-default qttools5-dev-tools'
+	sudo add-apt-repository ppa:beineri/opt-qt-5.12.0-xenial
 fi
 
-if [ "$BUILD_TYPE" = "doxygen" ]; then 
-	packages_list+=' doxygen'
-elif [ "$BUILD_TYPE" = "coverage" ]; then
-	packages_list+=' libqt5sql5-sqlite'
-fi
-
-sudo apt-get update -qq
+sudo apt-get update
--- .travis/linux/install.sh
+++ .travis/linux/install.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
+packages_list=''
+if [ "${CC}" = "clang" ]; then
+	packages_list='qt512-meta-minimal qt512tools'
+else
+	packages_list='qt5-default qttools5-dev-tools'
+fi
+
+if [ "$BUILD_TYPE" = "doxygen" ]; then 
+	packages_list+=' doxygen'
+elif [ "$BUILD_TYPE" = "coverage" ]; then
+	packages_list+=' libqt5sql5-sqlite'
+fi
 sudo apt-get install -y $packages_list
+
 
 if [ "$BUILD_TYPE" = "coverage" ]; then 
 	gem install lcoveralls
 fi
+
 
 if [ "$CC" = "gcc" ]; then
 	export USING_QT_MKSPEC="linux-g++-64"
