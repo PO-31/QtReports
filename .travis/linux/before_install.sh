c14e473f616f9904b1331202a412bb074ca18177
Alexander Khristosenko <dr.claws.sakh@gmail.com>
2019-02-17 16:58:43 +1000

    travis fix

--- .travis/linux/before_install.sh
+++ .travis/linux/before_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ "${CC}" = "clang" ]; then
-	sudo add-apt-repository ppa:beineri/opt-qt-5.12.0-xenial
+	sudo add-apt-repository -y ppa:beineri/opt-qt-5.12.0-xenial
 fi
 
 sudo apt-get update
