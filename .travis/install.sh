1019db4731f5d4e542147eb7ff51442da9ec05ed
Alexander Khristosenko <dr.claws.sakh@gmail.com>
2019-03-02 02:35:00 +1000

    changed mingw path

--- .travis/install.sh
+++ .travis/install.sh
@@ -2,15 +2,14 @@
 
 cd /c/
 "$HOME/packages/7z.exe" x "$HOME/packages/qtbase-build.7z"
+"$HOME/packages/7z.exe" x "$HOME/packages/mingw-w64.7z"
 
 if [[ $? -ne 0 ]]; then
 	exit 1
 fi
 
-cd "$HOME"
-"$HOME/packages/7z.exe" x "$HOME/packages/mingw-w64.7z"
 
 export QTDIR="/c/qtbase-build"
-export PATH="$QTDIR/bin:$HOME/mingw32/bin:$PATH"
+export PATH="$QTDIR/bin:/c/mingw32/bin:$PATH"
 
 cd "$TRAVIS_BUILD_DIR"
