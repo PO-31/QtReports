752b52d0f15412eb7cd02425c8b01eeb78a9854c
Alexander Khristosenko <dr.claws.sakh@gmail.com>
2019-03-02 01:10:07 +1000

    qtbase path changed [skip ci]

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-cd "$HOME"
-./packages/7z* x ./packages/qtbase-build.7z
+cd /c/
+"$HOME/packages/7z.exe" x "$HOME/packages/qtbase-build.7z"
 
 if [[ $? -ne 0 ]]; then
 	exit 1
 fi
 
-export QTDIR="$HOME/qtbase-build"
+export QTDIR="/c/qtbase-build"
 export PATH="$QTDIR/bin:$PATH"
 
 cd "$TRAVIS_BUILD_DIR"
