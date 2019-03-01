0fab91354f0afb2fab5807a60d6de09cab811c18
Alexander Khristosenko <dr.claws.sakh@gmail.com>
2019-03-01 13:44:34 +1000

    cache uploading fix

--- .travis/windows/install.sh
+++ .travis/windows/install.sh
@@ -3,9 +3,9 @@
 cd "$HOME"
 ./packages/7z.exe x ./packages/qtbase-build-win32.zip
 
-# powershell
-# Expand-Archive qtbase-build-win32.zip -DestinationPath ..\ -Force
-# exit
+if [[ $? -ne 0 ]]; then
+	exit 1
+fi
 
 export PATH="$HOME/qtbase-build/bin:$PATH"
 
