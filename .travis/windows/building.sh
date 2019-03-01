0e41c7c52c35fd557a09a2a745bedfcc77653d77
Alexander Khristosenko <dr.claws.sakh@gmail.com>
2019-03-02 01:32:30 +1000

    specify to our mingw32-make

--- .travis/windows/building.sh
+++ .travis/windows/building.sh
@@ -2,7 +2,7 @@
 
 cd "$TRAVIS_BUILD_DIR/qtreports"
 qmake -spec ${USING_QT_MKSPEC} "CONFIG+=${BUILD_TYPE}" qtreports.pro
-mingw32-make -j$(nproc)
+"$HOME/mingw32/bin/mingw32-make.exe" -j$(nproc)
 if [[ $? -ne 0 ]]; then
 	exit 1
 fi
