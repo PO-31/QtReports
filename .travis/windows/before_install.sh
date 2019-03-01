a78380a316fd535729960f614ecb6d1002989c19
Alexander Khristosenko <dr.claws.sakh@gmail.com>
2019-03-01 11:33:39 +1000

    package cache test

--- .travis/windows/before_install.sh
+++ .travis/windows/before_install.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 
+cd "$HOME/packages/.git"
+if [[ $? -ne 0 ]]; then
+	cd "$HOME"
+	rm -R -f "$HOME/packages/*"
+	git clone https://github.com/PO-31/packages/ "$HOME/packages/"
+else
+	cd "$HOME/packages"
+	git pull
+fi
+
+cd "$TRAVIS_BUILD_DIR"
--- .travis/windows/install.sh
+++ .travis/windows/install.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 
+cd "$HOME/packages"
+powershell -Command "& {Expand-Archive qtbase-build-win32.zip -DestinationPath $HOME}"
+if [[ $? -ne 0 ]]; then
+	echo "Error in qtbase extraction"
+	exit 1
+fi
+
+export PATH=$HOME/qtbase-build/bin:$PATH
+
+cd "$TRAVIS_BUILD_DIR"
