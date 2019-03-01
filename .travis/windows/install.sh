e8c5a5d5cd01a3d531281b0724fff781dca056d1
Alexander Khristosenko <dr.claws.sakh@gmail.com>
2019-03-01 13:13:51 +1000

    try 4

--- .travis/windows/install.sh
+++ .travis/windows/install.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
 cd "$HOME/packages"
-# powershell -Command "& {Expand-Archive qtbase-build-win32.zip -DestinationPath $HOME -Force}"
-# if [[ $? -ne 0 ]]; then
-	# echo "Error in qtbase extraction"
-	# exit 1
-# fi
-powershell
-Expand-Archive qtbase-build-win32.zip -DestinationPath ..\ -Force
-exit
+powershell -Command {Expand-Archive qtbase-build-win32.zip -DestinationPath "$HOME" -Force} -Version 5.0
+
+# powershell
+# Expand-Archive qtbase-build-win32.zip -DestinationPath ..\ -Force
+# exit
 
 export PATH="$HOME/qtbase-build/bin:$PATH"
 
