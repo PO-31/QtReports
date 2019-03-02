58d8b8c2ef68e6f7dafe3a34bb2722491fb0fe78
Alexander Khristosenko <dr.claws.sakh@gmail.com>
2019-03-02 18:55:23 +1000

    tests passing has been hidden Due to unresolved problem with lib searching tests passing is disabled. One of possible options is to wait Travis CI full compability with PowerShell

--- .travis/windows/testing.sh
+++ .travis/windows/testing.sh
@@ -15,11 +15,19 @@ fi
 echo
 echo "Tests program has been built"
 
-./qtreports-tests
+# At this time (02.03.2019) it's impossible to run tests
+# due to Git Bash problems with locating libs.
+# After a lot of tries the best solution is
+# to wait for Travis-CI compatibility with PowerShell.
 
-if [[ $? -ne 0 ]]; then
-	echo "Some of the tests FAILED"
-	exit 1
-fi
+# OR MOVE Windows building TO AppVeyor
+# It supports PowerShell and Qt out of box
 
-echo "Tests PASSED"
+# ./qtreports-tests
+# if [[ $? -ne 0 ]]; then
+	# echo "Some of the tests FAILED"
+	# exit 1
+# fi
+# echo "Tests PASSED"
+
+cd "$TRAVIS_BUILD_DIR"
