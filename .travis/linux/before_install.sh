8532cfa6146bfe580e3f7e3ab15fa2815510f6ad
Alexander Khristosenko <dr.claws.sakh@gmail.com>
2019-03-04 12:13:45 +1000

    old scripts' lines removed and report moved to right directory

--- .travis/linux/before_install.sh
+++ .travis/linux/before_install.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-# Because Xenial's repo doesn't have relatively new version
-# of Qt5 (since july 2015!) we have to use this repo.
-
-# sudo add-apt-repository -y ppa:beineri/opt-qt-5.12.0-xenial
-
-# sudo apt-get update
-
-
 git clone -b linux https://github.com/PO-31/packages/ "$HOME/packages/" --single-branch --depth 1
 
 if [[ $? -ne 0 ]]; then
